933116da89476a6ab5f057a2eef533ee3eef7303
Samuel-AKINGENEYE <freshtalent491@gmail.com>
2026-05-09 18:06:03 +0200

    Simplify User model and auth route

--- models/User.js
+++ models/User.js
@@ -2,40 +2,12 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  email: String,
+  password: String,
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-userSchema.methods.comparePassword = function (candidate) {
+userSchema.methods.comparePassword = function(candidate) {
   return bcrypt.compare(candidate, this.password);
 };
-
-// Never expose password in API responses
-userSchema.set('toJSON', {
-  transform: (_doc, ret) => {
-    delete ret.password;
-    return ret;
-  },
-});
 
 export default mongoose.model('User', userSchema);
